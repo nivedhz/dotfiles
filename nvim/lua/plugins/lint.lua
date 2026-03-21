9c0b4ddd847f4abca7977b2a3e213ba38fb97b7c
Nivedh E R <nivedher12@gmail.com>
2026-03-21 12:01:40 +0530

    added some plugins to lazyvim

---
+++ nvim/lua/plugins/lint.lua
@@ -0,0 +1,20 @@
+return {
+  "mfussenegger/nvim-lint",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local lint = require("lint")
+
+    lint.linters_by_ft = {
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      python = { "flake8" },
+      lua = { "luacheck" },
+    }
+
+    vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+  end,
+}
