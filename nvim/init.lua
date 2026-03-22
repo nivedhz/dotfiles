a3964aa3cc5fb80d812f8d41bb4d530d12ac7a5d
Nivedh E R <nivedher12@gmail.com>
2026-03-22 09:40:16 +0530

    removed the shortcut for nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+local handle = io.popen("bash -lc 'which node'")
+local node = handle:read("*a"):gsub("%s+", "")
+handle:close()
+
+if node ~= "" then
+  local node_dir = vim.fn.fnamemodify(node, ":h")
+  vim.env.PATH = node_dir .. ":" .. vim.env.PATH
+end
